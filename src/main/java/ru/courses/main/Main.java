ff7069c4e740e950fe26e6335c178748aea05827
Ramazan Aminov <RAminov@inno.tech>
2025-12-09 20:44:14 +0300

    Реализовано создание и вывод двух экземпляров точек координат

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,6 +1,14 @@
 package ru.courses.main;
 
+import ru.stepup.homeworks.geometry.Point;
+
+
 public class Main {
     public static void main(String[] args) {
+        java.awt.Point awtPoint= new java.awt.Point(10, 20);
+        System.out.println("AWT Point: " + awtPoint);
+
+        Point myPoint = new ru.stepup.homeworks.geometry.Point(30, 40);
+        System.out.println("My Point: " + myPoint);
     }
 }
